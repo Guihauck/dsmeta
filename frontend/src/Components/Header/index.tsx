c99f32c1439481a463dd9486113dd1abd102dfad
Guilherme Hauck <guihauck1998@gmail.com>
2023-03-27 21:48:00 -0300

    Other Components - Correction

---
+++ frontend/src/Components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="Infelizmente a foto não existe!" />
+                <h1>DSMeta</h1>
+                <p>Desenvolvido por
+                    <a href="https://www.instagram.com/guihauck1998/" target="_blank">@Guilherme Hauck</a>
+                </p>
+            </div>
+        </header>
+
+    )
+}
+
+export default Header
